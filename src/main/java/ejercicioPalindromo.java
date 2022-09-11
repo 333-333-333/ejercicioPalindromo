cc694be5229f3ffac68c99dc2e162eedf65d854f
333-333-333 <104991957+333-333-333@users.noreply.github.com>
2022-09-11 15:45:14 -0300

    Archivos subidos.

---
+++ src/main/java/ejercicioPalindromo.java
@@ -0,0 +1,47 @@
+import java.util.Locale;
+
+public class ejercicioPalindromo {
+
+    public static void main(String[] args) {
+
+    }
+
+    /* La función planteada en el ejercicio solamente consideraba sacarle los espacios a la string, e invertirlas, nada
+     más*/
+
+    public static boolean esPalindromo (String texto){
+
+        String temp = "";
+        temp = texto.replaceAll(" ","");
+
+        String retorno = "";
+
+        for (int i=0; i<temp.length(); i++)
+        {
+            char c = temp.charAt(i);
+            retorno = c + retorno;
+        }
+
+        return retorno.equals(texto);
+
+    }
+
+    public static boolean nuevoEsPalindromo(String texto) {
+
+        String textoAComparar = texto.replaceAll(" ","").toLowerCase();
+        StringBuilder retorno = new StringBuilder();
+
+        try {
+            for (int i=0; i<textoAComparar.length(); i++) {
+                char c = textoAComparar.charAt(i);
+                retorno.insert(0, c);
+            }
+            System.out.println(retorno);
+        } catch (ClassCastException e) {
+            System.out.println("El tipo de objeto no corresponde.");
+        }
+
+        return textoAComparar.equals(retorno.toString()) && retorno.length()>1;
+    }
+
+}
